2c1a07dd55d6db86a22ddb13e33586dbf494547a
Jonathan Hesler <jeh8443@rit.edu>
2024-12-17 15:20:24 -0500

    Initial Commit

---
+++ HangoutScheduler/HangoutSchedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HangoutSchedulerApp.swift
+//  HangoutScheduler
+//
+//  Created by Jonathan Hesler on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct HangoutSchedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HangoutSchedulerTests/HangoutSchedulerTests.swift
@@ -0,0 +1,17 @@
+//
+//  HangoutSchedulerTests.swift
+//  HangoutSchedulerTests
+//
+//  Created by Jonathan Hesler on 12/17/24.
+//
+
+import Testing
+@testable import HangoutScheduler
+
+struct HangoutSchedulerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
